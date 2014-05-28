1d4856e9ab2d73e748691934afdaa51b43c72dfb
cwelke <cwelke@cern.ch>
2014-05-28 11:35:40 -0700

    removing unnecessary files

--- badEventFilter/src/badEventFilter_cc.d
+++
@@ -1,6 +0,0 @@
-
-# DO NOT DELETE
-
-./src/badEventFilter_cc.so: /home/users/cwelke/dev/git/cwelke/looperTools/badEventFilter/./include/badEventFilter.h
-./src/badEventFilter_cc.so: /cvmfs/cms.cern.ch/slc5_amd64_gcc462/lcg/root/5.32.00-cms9/include/cintdictversion.h /cvmfs/cms.cern.ch/slc5_amd64_gcc462/lcg/root/5.32.00-cms9/include/RVersion.h
-badEventFilter_cc__ROOTBUILDVERSION= 5.32/00
